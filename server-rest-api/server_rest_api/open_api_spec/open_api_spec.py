e09a3398c7db7a74ced83e3002215d9d683711b8
Arnab Mukherjee <am0072008@gmail.com>
2024-09-03 14:04:07 +0530

    removing try it out feature

--- server-rest-api/server_rest_api/open_api_spec/open_api_spec.py
+++ server-rest-api/server_rest_api/open_api_spec/open_api_spec.py
@@ -3,8 +3,16 @@ from flask_swagger_ui import get_swaggerui_blueprint
 API_DOCS_URL = '/docs'
 DOCS_LOCATION = '/static/swagger.yaml'
 
+# swagger ui configs 
+# check this link: https://github.com/swagger-api/swagger-ui/blob/HEAD/docs/usage/configuration.md for list of other configs
+swagger_config = {
+    "tryItOutEnabled": False, # disables try it out feature
+    "supportedSubmitMethods": [] # disables try it out feature
+}
+
 # swagger blueprint object
 api_docs_bp = get_swaggerui_blueprint(
     API_DOCS_URL,
-    DOCS_LOCATION
+    DOCS_LOCATION,
+    config=swagger_config
 )
