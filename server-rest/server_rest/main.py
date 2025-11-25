393862c60c943b72e8e2b35c1e64b11bbf536fd8
Arnab Mukherjee <am0072008@gmail.com>
2025-11-25 22:16:15 +0530

    anniversary api root endpoint

--- server-rest/server_rest/main.py
+++ server-rest/server_rest/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 from server_rest.schemas.common import GenericMessage
+from server_rest.api.routes.anniversery import router as anniversery_router
+
+
 app = FastAPI()
 
 
 @app.get("/", response_model=GenericMessage)
 def read_root():
     return {"message": "Welcome to root endpoint of family server rest"}
+
+
+app.include_router(anniversery_router)
