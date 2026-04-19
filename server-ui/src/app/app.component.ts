f0bf07551347209adfcd17fe94277f302c81a349
Arnab Mukherjee <am0072008@gmail.com>
2026-04-19 22:21:50 +0530

    update to v19

--- server-ui/src/app/app.component.ts
+++ server-ui/src/app/app.component.ts
@@ -5,16 +5,15 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { EntryCommentComponent } from './components/entry-comment/entry-comment.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    CommonModule, 
-    RouterOutlet, 
-    NavBarComponent, 
-    EntryCommentComponent
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [
+        CommonModule,
+        RouterOutlet,
+        NavBarComponent,
+        EntryCommentComponent
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'server-ui';
